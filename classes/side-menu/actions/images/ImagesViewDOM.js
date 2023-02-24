62e27a7eb0544d23f8421f33a48cba49e7f1b6f5
wizzhi <wizz@msn.cn>
2023-02-24 13:44:39 +0800

    Update ImagesViewDOM.js

--- classes/side-menu/actions/images/ImagesViewDOM.js
+++ classes/side-menu/actions/images/ImagesViewDOM.js
@@ -15,7 +15,7 @@ class ImagesViewDOM {
                 <svg class="action-images-svg" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                 </div>
               <div class="action-images-text-container">
-                <p>Dodaj zdjecia</p>
+                <p>添加图片</p>
               </div>
               </label>
               <input type="file" id="action-images-importer" class="action-images-importer" name="action-images-importer" multiple>
