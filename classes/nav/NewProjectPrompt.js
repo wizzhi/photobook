96a8a486217bfbceb0756ad0c7dd4976687b5c3c
wizzhi <wizz@msn.cn>
2023-02-24 13:21:05 +0800

    Update NewProjectPrompt.js

--- classes/nav/NewProjectPrompt.js
+++ classes/nav/NewProjectPrompt.js
@@ -22,16 +22,14 @@ class NewProjectDialog {
 
     promptTemplate = `
         <div class="new-project-prompt">
-            <h2 class="new-project-title">Nowy projekt</h2>
-            <p class="new-project-message">Stworzenie nowego projektu nadpisze dotychczasowy. Upewnij się, że praca
-                została
-                wyeksportowana. W&nbsp;przeciwnym wypadku wszystkie zmiany zostaną utracone.
+            <h2 class="new-project-title">新建相册</h2>
+            <p class="new-project-message">新建相册将会清除现在的所有修改！
                 <br /> <br />
-                Na pewno chcesz stworzyć nowy projekt?
+                确定是要新建吗?
             </p>
             <div class="new-project-button-wrapper">
-                <button class="answer-yes">Tak</button>
-                <button class="answer-no">Nie</button>
+                <button class="answer-yes">新建</button>
+                <button class="answer-no">暂不</button>
             </div>
         </div>`;
 }
